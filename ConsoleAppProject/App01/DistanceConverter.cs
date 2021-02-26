72d5588ea82c44501658c1738a57270ee6c0a2f4
Andrew Wood <100108564@student.burnley.ac.uk>
2021-02-26 12:36:05 +0000

    added feet and meters to the code

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -15,22 +15,42 @@ namespace ConsoleAppProject.App01
     {
         public const int FEET_IN_MILES = 5280;
 
+        public const double METERS_IN_MILES = 1609.34;
+
         // Distance measured in miles
         private double miles;
         // Distance measured in feet
         private double feet;
+        // Distance measured in meters
+        private double meters;
 
         /// <summary>
         /// This method will input the distance measured in miles
         /// calculate the same distance in feeet, and output the 
         /// distance in feet.
         /// </summary>
-        public void Run()
+        public void MilesToFeet()
         {
             OutputHeading();
             InputMiles();
             CalculateFeet();
             OutputFeet();
+        }
+
+        public void FeetToMiles()
+        {
+            OutputHeading();
+            InputFeet();
+            CalculateMiles();
+            OutputMiles();
+        }
+
+        public void MilesToMeters()
+        {
+            OutputHeading();
+            InputMiles();
+            CalculateMeters();
+            OutputMeters();
         }
 
         private void OutputHeading()
@@ -47,22 +67,73 @@ namespace ConsoleAppProject.App01
             /// Prompt the user to enter the distance in miles
             /// Input the miles as a double number
             /// </summary>
-            private void InputMiles()
+            
+        private void InputMiles()
         {
             Console.Write("Please enter the number of miles > ");
             string value = Console.ReadLine();
             miles = Convert.ToDouble(value);
         }
-
-        private void CalculateFeet()
+            
+        private void InputMeters()
         {
-            feet = miles * 5280;
+            Console.Write("Please enter the number of meters > ");
+            string value = Console.ReadLine();
+            meters = Convert.ToDouble(value);
         }
 
+        private void InputFeet()
+        {
+            Console.Write("Please enter the number of feet > ");
+            string value = Console.ReadLine();
+            feet = Convert.ToDouble(value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateFeet()
+        {
+            feet = miles * FEET_IN_MILES;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateMiles()
+        {
+            miles = feet / FEET_IN_MILES;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateMeters()
+        {
+            meters = miles * METERS_IN_MILES;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         private void OutputFeet()
         {
             Console.WriteLine(miles + " miles is " + feet + " feet!");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OutputMiles()
+        {
+            Console.WriteLine(miles + " feet is " + miles + " miles!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OutputMeters()
+        {
+            Console.WriteLine(miles + " miles is " + meters + " meters!");
+        }
     }
 }
